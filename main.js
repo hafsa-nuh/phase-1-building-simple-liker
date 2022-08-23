c57fde2c686f7e5c138de33c2dda4db7d1ed8d70
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-08-23 23:34:16 +0300

    done

--- main.js
+++ main.js
@@ -5,6 +5,33 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(event) {
+  const heart = event.target;
+  mimicServerCall()
+    .then(() => {
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(error => {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
+
+for (const glyph of articleHearts) {
+  // glyph.addEventListener("click", heart);
+  glyph.addEventListener("click", likeCallback);
+}
+
 
 
 //------------------------------------------------------------------------------
